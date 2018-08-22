8a3ae3655840d4081e48691e1808c5b4b4668ee8
Adhiv Dhar <adhivd@gmail.com>
2018-08-22 12:23:32 -0700

    modded the migrations so duplicate email column is gone

--- db/schema.rb
+++ db/schema.rb
@@ -47,9 +47,9 @@ ActiveRecord::Schema.define(version: 2018_04_22_173942) do
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -63,7 +63,6 @@ ActiveRecord::Schema.define(version: 2018_04_22_173942) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
-    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
